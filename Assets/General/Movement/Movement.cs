76f0f3a06f84e9d4746ccfc14a0c322f9d474f70
Dawidan-orig <andrey9davy@gmail.com>
2023-09-14 11:19:57 +0300

    Slope and ground snap
    - Теперь всё, у чего есть компонент Movement умеет "Прилипать" к земле, из-за чего управляемость повысилась в разы

--- Assets/General/Movement/Movement.cs
+++ Assets/General/Movement/Movement.cs
@@ -7,9 +7,14 @@ public class Movement : MonoBehaviour
     public float walkSpeed = 5;
     public float runSpeed = 10;
     public float sprintSpeed = 20;
-    public float VelociyAngle_ChangeSpeedEuler = 30;
+    public float maximumReachableSpeed = 25; // Максимальная возможная скорость, которую может развить этот Rigidbody, и которую будет уменьшать до спринта
+    public float ragdollSpeed = 35; // Скорость, после которой персонаж спотыкается и падает в Ragdoll
+    public float velociyAngleChangeSpeedEuler = 90;
     public float distToGround = 0.3f;
-    public float dragOnGround = 1;
+    public float activeDrag = 20;
+    public float passiveDragOnGround = 1;
+    public float snapHeight = 3;
+    public float snapSpeed = 21;
     public float toGroundForce = 5;
     [Header("Air")]
     public float dragOffGround = 0.2f;
@@ -38,6 +43,10 @@ public class Movement : MonoBehaviour
     bool _jumpReady = true;
     [SerializeField]
     protected Vector2 _inputMovement;
+    [SerializeField]
+    protected int _framesSinceLastGrounded = 0;
+    [SerializeField]
+    protected Vector3 _contactNormal= Vector3.zero;
     protected Rigidbody _rb;
 
     public enum SpeedType
@@ -61,7 +70,7 @@ public class Movement : MonoBehaviour
     protected virtual void Update()
     {
         if (_inputMovement == Vector2.zero)
-            _rb.drag = _isGrounded ? dragOnGround : dragOffGround;
+            _rb.drag = _isGrounded ? passiveDragOnGround : dragOffGround;
         else
             _rb.drag = 0;
 
@@ -71,7 +80,8 @@ public class Movement : MonoBehaviour
     protected virtual void FixedUpdate()
     {
         _isGrounded = Physics.BoxCast(vital.bounds.center, new Vector3(vital.bounds.size.x / 2, 0.1f, vital.bounds.size.z / 2),
-            transform.up * -1, out _, transform.rotation, vital.bounds.size.y / 2 + distToGround, ground);
+            transform.up * -1, out RaycastHit hit, transform.rotation, vital.bounds.size.y / 2 + distToGround, ground);
+        _contactNormal = hit.normal;
 
         if (_inJump && _isGrounded && _rb.velocity.y < 0)
             _inJump = false;
@@ -80,6 +90,16 @@ public class Movement : MonoBehaviour
 
         if (IsGrounded)
             FixMovement();
+
+        _framesSinceLastGrounded++;
+        if (_isGrounded || SnapToGround())
+        {
+            _framesSinceLastGrounded = 0;
+            _contactNormal.Normalize();
+        }
+        else
+            _contactNormal = Vector3.up;
+
     }
 
     public void PassInput(Vector2 inputMovement, SpeedType type, bool jump)
@@ -92,8 +112,6 @@ public class Movement : MonoBehaviour
             _moveSpeed = runSpeed;
         else
             _moveSpeed = walkSpeed;
-
-
 
         if (jump)
             Jump();
@@ -110,8 +128,8 @@ public class Movement : MonoBehaviour
         {
             _rb.AddForce(SlopeMoveDir() * _moveSpeed, ForceMode.Acceleration);
 
-            if (_rb.velocity.y > 0)
-                _rb.AddForce(Vector3.down * toGroundForce, ForceMode.Acceleration);
+            //if (_rb.velocity.y > 0)
+            //    _rb.AddForce(Vector3.down * toGroundForce, ForceMode.Acceleration);
 
             ControlVelocityAngle(SlopeMoveDir());
 
@@ -133,7 +151,7 @@ public class Movement : MonoBehaviour
         if (_inputMovement != Vector2.zero)
         {
             Vector3 rotation = Vector3.RotateTowards(_rb.velocity.normalized, desireDir,
-                VelociyAngle_ChangeSpeedEuler * Mathf.Deg2Rad * Time.deltaTime, _moveSpeed);
+                velociyAngleChangeSpeedEuler * Mathf.Deg2Rad * Time.deltaTime, _moveSpeed);
             _rb.velocity = rotation.normalized * _rb.velocity.magnitude;
         }
     }
@@ -142,17 +160,42 @@ public class Movement : MonoBehaviour
     {
         if (OnSlope() && _rb.velocity.magnitude > _moveSpeed)
         {
-            _rb.velocity = _rb.velocity.normalized * _moveSpeed;
+            _rb.velocity = _rb.velocity.normalized * (_rb.velocity.magnitude - activeDrag * Time.fixedDeltaTime);
+
+            if (_rb.velocity.magnitude > maximumReachableSpeed)
+                _rb.velocity = _rb.velocity.normalized * maximumReachableSpeed;
             return;
         }
 
         Vector3 flatVelocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
-
         if (flatVelocity.magnitude > _moveSpeed)
         {
-            _rb.velocity = flatVelocity.normalized * _moveSpeed + _rb.velocity.y * Vector3.up;
+            _rb.velocity = flatVelocity.normalized * (flatVelocity.magnitude - activeDrag * Time.fixedDeltaTime) + _rb.velocity.y * Vector3.up;
+            if (_rb.velocity.magnitude > maximumReachableSpeed)
+                _rb.velocity = flatVelocity.normalized * maximumReachableSpeed + _rb.velocity.y * Vector3.up;
         }
     }
+
+    protected virtual bool SnapToGround()
+    {
+        if (_inJump)
+            return false;
+        if (_rb.velocity.magnitude > snapSpeed)
+            return false;
+        if (_framesSinceLastGrounded > 1)
+            return false;
+        if (!Physics.Raycast(vital.bounds.center, Vector3.down, out RaycastHit hit, snapHeight, ground))
+            return false;
+        //if(hit.normal.y < minGroundDotProduct)
+        //    return false;
+
+        _contactNormal = hit.normal;
+        float dot = Vector3.Dot(_rb.velocity, hit.normal);
+        if(dot > 0)
+            _rb.velocity = (_rb.velocity - hit.normal * dot).normalized * _rb.velocity.magnitude;
+        return true;
+    }
+
 
     protected virtual void Jump()
     {
@@ -169,6 +212,7 @@ public class Movement : MonoBehaviour
     {
         if (Physics.Raycast(vital.bounds.center, transform.up * -1, out var hit, vital.bounds.size.y / 2 + distToGround, ground))
         {
+            _contactNormal = hit.normal;
             _slopeHit = hit;
             float angle = Vector3.Angle(Vector3.up, hit.normal);
             return angle < slopeAngle && angle != 0;
